7123e22000dff76d61cfe38f66f41a2e0ba2ffa0
enkaybit <84878530+enkaybit@users.noreply.github.com>
2023-05-23 09:09:28 -0700

    Update youconvert-idea

--- youconvert.py
+++ youconvert.py
@@ -1,1 +1,50 @@
+# WIP
+# Use the youtube_transcript_api module to extract the transcripts:
+from youtube_transcript_api import YouTubeTranscriptApi
 
+video_id = 'VIDEO_ID' # Replace VIDEO_ID with the ID of the video
+transcript = YouTubeTranscriptApi.get_transcript(video_id)
+
+# Clean and preprocess the transcripts using NLP techniques such as tokenization, stemming, and lemmatization.
+
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+
+# Tokenize the text
+tokens = word_tokenize(transcript)
+
+# Lemmatize the tokens
+lemmatized_tokens = [lemmatizer.lemmatize(token) for token in tokens]
+
+
+# Train a chatbot using the preprocessed transcripts. You can use libraries like chatterbot  or tensorflow to train a chatbot.
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+chatbot = ChatBot('My Chatbot')
+
+trainer = ListTrainer(chatbot)
+
+# Train the chatbot using the preprocessed transcripts
+trainer.train(lemmatized_tokens)
+
+# Run the chatbot in a Jupyter notebook using the  ipywidgets module to create a user interface.
+
+import ipywidgets as widgets
+from IPython.display import display
+
+text_box = widgets.Text(description='User Input:')
+display(text_box)
+
+def get_response(sender):
+    user_input = text_box.value
+    response = chatbot.get_response(user_input)
+    print(response)
+
+button = widgets.Button(description='Get Response')
+button.on_click(get_response)
+display(button)
